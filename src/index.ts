574bb5124d6989855e0e9636d2d6f849de7c5949
Matthew Wright <matthew@smallhelm.com>
2019-03-22 16:05:42 -0500

    exporting more interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+import {
+  ChannelConfig,
+  ChannelReadOnly,
+  EventPolicy,
+  EventPolicyRule,
+  QueryPolicy,
+  QueryPolicyRule
+} from "./Channel";
+import { PicoEvent } from "./PicoEvent";
 import { PicoFramework, PicoFrameworkConf } from "./PicoFramework";
+import { PicoQuery } from "./PicoQuery";
+import { Ruleset, RulesetConfig, RulesetInstance } from "./Ruleset";
 
-export { PicoFramework, PicoFrameworkConf };
+export {
+  PicoEvent,
+  PicoQuery,
+  PicoFramework,
+  PicoFrameworkConf,
+  ChannelConfig,
+  RulesetInstance,
+  Ruleset,
+  RulesetConfig,
+  ChannelReadOnly,
+  EventPolicy,
+  EventPolicyRule,
+  QueryPolicy,
+  QueryPolicyRule
+};
